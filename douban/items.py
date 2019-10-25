605fb93e60d809ad386db2c9b9cbca68e29f2793
Zhijie Huang <t-zhhu@microsoft.com>
2019-10-25 11:39:59 +0800

    comment spider done

--- douban/items.py
+++ douban/items.py
@@ -1,11 +1,12 @@
 from scrapy.item import Item, Field
 
-class DoubanCrawlerItem(Item):
+class DoubanCommentItem(Item):
     user_name = Field()
-    user_url = Field()
+    user_id = Field()
     content = Field()
     votes = Field()
     user_status = Field()
     rating = Field()
     comment_time = Field()
     comment_id = Field()
+    movie_id = Field()
--- douban/spiders/comments_spider.py
+++ douban/spiders/comments_spider.py
@@ -15,29 +15,31 @@ class CommentsSpider(Spider):
         self.count = 0
 
     def start_requests(self):
-        urls = [
-            'https://movie.douban.com/subject/26683723/comments?start=0&limit=20&sort=new_score&status=P',
-        ]
-
-        for url in urls:
-            # self.pages.append(url)
-            yield Request(url=url, callback=self.parse_comments)
+        with open('index.txt') as f:
+            for i in f:
+                url = 'https://movie.douban.com/subject/' + i.rstrip('\n') + '/comments?start=0&limit=20&sort=new_score&status=P'
+                # self.pages.append(url)
+                yield Request(url=url, callback=self.parse_comments)
 
     def parse_comments(self, response):
-        # _setDNSCache()
+        _setDNSCache()
 
         # crawl comments
         comment = DoubanCrawlerItem()
         # print(response.body)
         for item in response.xpath('//div[@class="comment-item"]'):
+            # 短评对应的电影id
+            comment['movie_id'] = response.url.split('/')[4]
             # 短评的唯一id
-            comment['comment_id'] = int(item.xpath('div[@class="comment"]/h3/span[@class="comment-vote"]/input/@value').extract()[0].strip())
+            comment['comment_id'] = item.xpath('div[@class="comment"]/h3/span[@class="comment-vote"]/input/@value').extract()[0].strip()
             # 多少人评论有用
             comment['votes'] = item.xpath('div[@class="comment"]/h3/span[@class="comment-vote"]/span/text()').extract()[0].strip()
             # 状态
-            comment['user_status'] = item.xpath('div[@class="comment"]/h3/span[@class="comment-info"]/span[1]/text()').extract()[0].strip()
+            # comment['user_status'] = item.xpath('div[@class="comment"]/h3/span[@class="comment-info"]/span[1]/text()').extract()[0].strip()
             # 评分
             comment['rating'] = item.xpath('div[@class="comment"]/h3/span[@class="comment-info"]/span[2]/@class').extract()[0].strip()
+            if comment['rating'] == 'comment-time':
+                comment['rating'] = 'none'
             # 评论时间
             comment['comment_time'] = item.xpath('div[@class="comment"]/h3/span[@class="comment-info"]/span[@class="comment-time "]/@title').extract()
             # 评论内容
@@ -45,7 +47,7 @@ class CommentsSpider(Spider):
             # 评论者名字（唯一）
             comment['user_name'] = item.xpath('div[@class="avatar"]/a/@title').extract()[0]
             # 评论者页面
-            comment['user_url'] = item.xpath('div[@class="avatar"]/a/@href').extract()[0]
+            comment['user_id'] = item.xpath('div[@class="avatar"]/a/@href').extract()[0].split('/')[4]
 
             yield comment
 
