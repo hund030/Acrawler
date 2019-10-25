893f1e08787ab55fdba5691d523eaef7151f1cde
Zhijie Huang <t-zhhu@microsoft.com>
2019-10-25 17:02:36 +0800

    movies page crawler done

--- douban/items.py
+++ douban/items.py
@@ -10,3 +10,32 @@ class DoubanCommentItem(Item):
     comment_time = Field()
     comment_id = Field()
     movie_id = Field()
+
+class DoubanMovieItem(Item):
+    movie_id = Field()
+    movie_name = Field()
+    director = Field()
+    author = Field()
+    actors = Field()
+    movie_type = Field()
+    official_website = Field()
+    region_made = Field()
+    language = Field()
+    date_published = Field()
+    movie_length = Field()
+    alias = Field()
+
+    votes = Field()
+    average_rating = Field()
+    stars5_ratings = Field()
+    stars4_ratings = Field()
+    stars3_ratings = Field()
+    stars2_ratings = Field()
+    stars1_ratings = Field()
+
+    description = Field()
+    awards = Field() # hold on
+    recommendations = Field()
+    labels = Field()
+    collections = Field()
+    wishes = Field()
